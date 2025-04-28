9ae51b1072e0ce0bf53afd8a17d05a7e75ae3a85
Murtaza Sogiyawla <murtazasogiyawla@Murtazas-MacBook-Air.local>
2025-04-28 10:48:39 +0530

    dataset added

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -8,12 +8,20 @@ def load_data(petrol_price_path: str, sales_data_path: str):
     sales_df = pd.read_csv("data/product_sales.csv")
     return petrol_df, sales_df
 
-def preprocess_data(petrol_df: pd.DataFrame, sales_df: pd.DataFrame):
-    """Prepare the final dataset for training."""
-    
-    # Merge datasets if needed (based on date or state if available)
-    # For now, assume sales_df is independent
-    
+
+def preprocess_data(petrol_df: pd.DataFrame, sales_df: pd.DataFrame) -> pd.DataFrame:
+    petrol_df = petrol_df.copy()
+    petrol_df.dropna(inplace=True)
+    petrol_df.reset_index(drop=True, inplace=True)
+
+    sales_df = sales_df.copy()
+    sales_df.dropna(inplace=True)
+    sales_df.reset_index(drop=True, inplace=True)
+
+    final_df = pd.concat([petrol_df, sales_df], axis=1)
+
+    return final_df
+
     # Feature Engineering example: we can later add moving averages, trends etc
     sales_df['Profit_Margin_Percent'] = (sales_df['Profit_Per_Unit'] / sales_df['Selling_Price']) * 100
     
